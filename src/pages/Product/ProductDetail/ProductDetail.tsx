4779900097892dcbf845801f5eb59419cb8b93f6
Xuan Thang <thangijr@gmail.com>
2024-10-08 15:18:47 +0700

    update

--- src/pages/Product/ProductDetail/ProductDetail.tsx
+++ src/pages/Product/ProductDetail/ProductDetail.tsx
@@ -17,8 +17,17 @@ import {
     Indicator,
     Rating,
     ScrollArea,
+    Tabs,
 } from "@mantine/core";
-import { IconMinus, IconPlus } from "@tabler/icons-react";
+import {
+    IconMessageCircle,
+    IconMinus,
+    IconPhoto,
+    IconPlus,
+    IconSettings,
+    IconTableSpark,
+    IconTir,
+} from "@tabler/icons-react";
 
 const ChiTietSP = () => {
     const [activeTab, setActiveTab] = useState("warranty");
@@ -332,96 +341,220 @@ const ChiTietSP = () => {
                                     </div>
                                 </Flex>
                             </div>
+                            <div className="my-[10px]">
+                                <Tabs defaultValue="gallery">
+                                    <Tabs.List>
+                                        <Tabs.Tab
+                                            value="gallery"
+                                            leftSection={<IconTableSpark />}
+                                        >
+                                            Bảo hành
+                                        </Tabs.Tab>
+                                        <Tabs.Tab
+                                            value="messages"
+                                            leftSection={<IconTir />}
+                                        >
+                                            Vận chuyển
+                                        </Tabs.Tab>
+                                    </Tabs.List>
+
+                                    <Tabs.Panel value="gallery">
+                                        <div className="warranty-content">
+                                            <ul>
+                                                <li>
+                                                    Các sản phẩm nội thất tại
+                                                    Mordren Home đa số đều được sản
+                                                    xuất tại nhà máy của công ty
+                                                    cổ phần xây dựng kiến trúc
+                                                    AA với đội ngũ nhân viên và
+                                                    công nhân ưu tú cùng cơ sở
+                                                    vật chất hiện đại
+                                                    (http://www.aacorporation.com/).
+                                                    Mordren Home đã kiểm tra kỹ
+                                                    lưỡng từ nguồn nguyên liệu
+                                                    cho đến sản phẩm hoàn thiện
+                                                    cuối cùng.
+                                                </li>
+                                                <li>
+                                                    Mordren Home bảo hành một năm
+                                                    cho các trường hợp có lỗi về
+                                                    kỹ thuật trong quá trình sản
+                                                    xuất hay lắp đặt.
+                                                </li>
+                                                <li>
+                                                    Quý khách không nên tự sửa
+                                                    chữa mà hãy báo ngay cho Nhà
+                                                    Xinh qua hotline: 1800 7200.
+                                                </li>
+                                                <li>
+                                                    Sau thời gian hết hạn bảo
+                                                    hành, nếu quý khách có bất
+                                                    kỳ yêu cầu hay thắc mắc thì
+                                                    vui lòng liên hệ với Nhà
+                                                    Xinh để được hướng dẫn và
+                                                    giải quyết các vấn đề gặp
+                                                    phải.
+                                                </li>
+                                            </ul>
+                                            <p>
+                                                TUY NHIÊN Mordren Home KHÔNG BẢO
+                                                HÀNH CHO CÁC TRƯỜNG HỢP SAU:
+                                            </p>
+                                            <ul>
+                                                <li>
+                                                    Khách hàng tự ý sửa chữa khi
+                                                    sản phẩm bị trục trặc mà
+                                                    không báo cho Mordren Home.
+                                                </li>
+                                                <li>
+                                                    Sản phẩm được sử dụng không
+                                                    đúng quy cách của sổ bảo
+                                                    hành (được trao gửi khi quý
+                                                    khách mua sản phẩm) gây nên
+                                                    trầy xước, móp, dơ bẩn hay
+                                                    mất màu.
+                                                </li>
+                                                <li>
+                                                    Sản phẩm bị biến dạng do môi
+                                                    trường bên ngoài bất bình
+                                                    thường (quá ẩm, quá khô, mối
+                                                    hay do tác động từ các thiết
+                                                    bị điện nước, các hóa chất
+                                                    hay dung môi khách hàng sử
+                                                    dụng không phù hợp).
+                                                </li>
+                                                <li>
+                                                    Sản phẩm hết hạn bảo hành.
+                                                </li>
+                                                <li>
+                                                    Sản phẩm không có phiếu bảo
+                                                    hành của Mordren Home.
+                                                </li>
+                                                <li>
+                                                    Xem nội dung sổ bảo hành
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </Tabs.Panel>
+
+                                    <Tabs.Panel value="messages">
+                                        <div className="shipping-content">
+                                            <ul>
+                                                <li>
+                                                    Mordren Home cung cấp dịch vụ
+                                                    giao hàng tận nơi, lắp ráp
+                                                    và sắp xếp vị trí theo đúng
+                                                    ý muốn của quý khách:
+                                                </li>
+                                                <li>
+                                                    MIỄN PHÍ giao hàng trong các
+                                                    Quận nội thành Tp.Hồ Chí
+                                                    Minh và Hà Nội, áp dụng cho
+                                                    các đơn hàng trị giá trên 10
+                                                    triệu.
+                                                </li>
+                                                <li>
+                                                    Đối với khu vực các tỉnh lân
+                                                    cận: Tính phí hợp lý theo
+                                                    dựa trên quãng đường vận
+                                                    chuyển
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </Tabs.Panel>
+                                </Tabs>
+                            </div>
                         </div>
                     </div>
-                    {/* <div>
-                    <div className="product-title-1">
-                        <p>Có Thể Bạn Cũng Thích</p>
-                    </div>
-                    <ListProducts />
-                </div>
-                <div>
-                    <div className="product-title-2">
-                        <h2>SẢN PHẨM VỪA XEM</h2>
-                        <div className="container">
-                            <div className="left">
-                                <img
-                                    src={banner_footer6}
-                                    alt="Sofa Image"
-                                    className="image-left"
-                                />
-                            </div>
-
-                            <div className="middle">
-                                <div className="content">
-                                    <h2>Mẫu thiết kế phòng khách</h2>
-                                    <p>
-                                        Phòng khách là không gian chính của ngôi
-                                        nhà, là nơi sum họp gia đình
-                                    </p>
-                                    <a href="#">MẪU PHÒNG KHÁCH →</a>
-                                </div>
-                                <div className="content">
-                                    <h2>Đồ trang trí</h2>
-                                    <p>
-                                        Mang lại những nguồn cảm hứng và nét
-                                        sinh động cho không gian
-                                    </p>
-                                    <a href="#">KHÁM PHÁ →</a>
-                                </div>
-                            </div>
-
-                            <div className="right">
-                                <img
-                                    src={banner}
-                                    alt="Decor Image"
-                                    className="image-right"
-                                />
-                            </div>
+                    <div>
+                        <div className="product-title-1">
+                            <p>Có Thể Bạn Cũng Thích</p>
                         </div>
-                        <div className="design-examples">
-                            <div className="design-example design-example-1">
-                                <div className="image-container">
+                        <ListProducts />
+                    </div>
+                    <div className="">
+                        <div className="product-title-2 ">
+                            <h2>Mẫu thiết kế</h2>
+                            <div className="container">
+                                <div className="left">
                                     <img
-                                        src={banner_footer2}
-                                        alt="Mẫu thiết kế phòng ngủ"
+                                        src={banner_footer6}
+                                        alt="Sofa Image"
+                                        className="image-left"
                                     />
                                 </div>
-                                <div className="content-container">
-                                    <h3>Mẫu thiết kế phòng ngủ</h3>
-                                    <p>
-                                        Những mẫu phòng ngủ của Nhà Xinh mang
-                                        đến cảm giác ấm cúng, gần gũi và thoải
-                                        mái
-                                    </p>
-                                    <a href="#" className="read-more">
-                                        MẪU PHÒNG NGỦ →
-                                    </a>
-                                </div>
-                            </div>
 
-                            <div className="design-example design-example-2">
-                                <div className="image-container">
+                                <div className="middle">
+                                    <div className="content">
+                                        <h2>Mẫu thiết kế phòng khách</h2>
+                                        <p>
+                                            Phòng khách là không gian chính của
+                                            ngôi nhà, là nơi sum họp gia đình
+                                        </p>
+                                        <a href="#">MẪU PHÒNG KHÁCH →</a>
+                                    </div>
+                                    <div className="content">
+                                        <h2>Đồ trang trí</h2>
+                                        <p>
+                                            Mang lại những nguồn cảm hứng và nét
+                                            sinh động cho không gian
+                                        </p>
+                                        <a href="#">KHÁM PHÁ →</a>
+                                    </div>
+                                </div>
+
+                                <div className="right">
                                     <img
-                                        src={banner_footer1}
-                                        alt="Mẫu thiết kế phòng ăn"
+                                        src={banner}
+                                        alt="Decor Image"
+                                        className="image-right"
                                     />
                                 </div>
-                                <div className="content-container">
-                                    <h3>Mẫu thiết kế phòng ăn</h3>
-                                    <p>
-                                        Một bữa ăn ngon luôn là mong ước của mỗi
-                                        gia đình. Không gian phòng ăn đóng vai
-                                        trò rất quan trọng trong văn hóa Việt.
-                                    </p>
-                                    <a href="#" className="read-more">
-                                        MẪU PHÒNG ĂN →
-                                    </a>
+                            </div>
+                            <div className="design-examples">
+                                <div className="design-example design-example-1">
+                                    <div className="image-container">
+                                        <img
+                                            src={banner_footer2}
+                                            alt="Mẫu thiết kế phòng ngủ"
+                                        />
+                                    </div>
+                                    <div className="content-container">
+                                        <h3>Mẫu thiết kế phòng ngủ</h3>
+                                        <p>
+                                            Những mẫu phòng ngủ của Mordren Home
+                                            mang đến cảm giác ấm cúng, gần gũi
+                                            và thoải mái
+                                        </p>
+                                        <a href="#" className="read-more">
+                                            MẪU PHÒNG NGỦ →
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="design-example design-example-2">
+                                    <div className="image-container">
+                                        <img
+                                            src={banner_footer1}
+                                            alt="Mẫu thiết kế phòng ăn"
+                                        />
+                                    </div>
+                                    <div className="content-container">
+                                        <h3>Mẫu thiết kế phòng ăn</h3>
+                                        <p>
+                                            Một bữa ăn ngon luôn là mong ước của
+                                            mỗi gia đình. Không gian phòng ăn
+                                            đóng vai trò rất quan trọng trong
+                                            văn hóa Việt.
+                                        </p>
+                                        <a href="#" className="read-more">
+                                            MẪU PHÒNG ĂN →
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div> */}
                 </div>
             </div>
         </>
