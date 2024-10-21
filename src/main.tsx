89ba1861c99256416a826e091aba88f849cf64cb
Xuan Thang <thangijr@gmail.com>
2024-10-21 22:37:34 +0700

    cài đặt maintine react table

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "../src/Styles/global.css";
+import "@mantine/core/styles.css"; //import Mantine V7 styles needed by MRT
+import "@mantine/dates/styles.css"; //if using mantine date picker features
+import "mantine-react-table/styles.css"; //import MRT styles
+import App from "./App.tsx";
 
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 const queryClient = new QueryClient();
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <MantineProvider>
+        <MantineProvider theme={theme}>
             <Notifications />
             <ModalsProvider>
                 <QueryClientProvider client={queryClient}>
