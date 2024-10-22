514ba3a8d5c02c2e6198a72b0de0f6eca9d1b9bd
NguyenVan-Truong <truongha21062004@gmail.com>
2024-10-22 22:50:01 +0700

    order

--- src/pages/ProfileUser/pages.tsx
+++ src/pages/ProfileUser/pages.tsx
@@ -16,9 +16,9 @@ const ProfileUser = () => {
             <div className="container !mt-5">
                 <hr />
             </div>
-            <div className="container grid grid-cols-1 lg:grid-cols-[25%_75%] !mt-6 !pb-5">
+            <div className="container grid grid-cols-1 lg:grid-cols-[25%_75%] xl:gap-5 xl:grid-cols-[20%_80%] !mt-6 !pb-5">
                 <div className="w-64 border-2 hidden lg:block bg-white">
-                    <div className="flex items-center space-x-2 justify-center mx-auto py-2">
+                    <div className="flex items-center space-x-2 mt-5 justify-center mx-auto py-2">
                         <Image
                             src={Avatar}
                             radius="xl"
@@ -27,11 +27,11 @@ const ProfileUser = () => {
                             fit="contain"
                         />{" "}
                         <Box w={160}>
-                            <Text truncate="end" size="sm">
+                            <Text truncate="end" size="lg">
                                 Nguyễn Văn Trường
                             </Text>
                             <Text
-                                size="sm"
+                                size="md"
                                 className="flex items-center !text-[#9B9B9B]"
                             >
                                 <IconPencil color="#9B9B9B" size={16} />
@@ -42,34 +42,58 @@ const ProfileUser = () => {
                     <hr />
                     <ul className="flex flex-col space-y-2 p-4">
                         <NavLink
-                            leftSection={<IconUser size="1rem" />}
+                            leftSection={<IconUser size="1.4rem" />}
                             href="/nguoi-dung/thong-tin-tai-khoan"
-                            label="Thông tin cá nhân"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Thông tin cá nhân
+                                </span>
+                            }
                         />
                         <NavLink
-                            leftSection={<IconShoppingCart size="1rem" />}
+                            leftSection={<IconShoppingCart size="1.3rem" />}
                             href="/nguoi-dung/don-hang"
-                            label="Danh sách đơn hàng"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Danh sách đơn hàng
+                                </span>
+                            }
                         />
                         <NavLink
-                            leftSection={<IconMapPin size="1rem" />}
+                            leftSection={<IconMapPin size="1.3rem" />}
                             href="/nguoi-dung/thong-tin-tai-khoan"
-                            label="Quản lý địa chỉ"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Quản lý địa chỉ
+                                </span>
+                            }
                         />
                         <NavLink
-                            leftSection={<IconCards size="1rem" />}
+                            leftSection={<IconCards size="1.3rem" />}
                             href="/nguoi-dung/thong-tin-tai-khoan"
-                            label="Danh sách thẻ tín dụng"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Danh sách thẻ tín dụng
+                                </span>
+                            }
                         />
                         <NavLink
-                            leftSection={<IconHeart size="1rem" />}
+                            leftSection={<IconHeart size="1.3rem" />}
                             href="/nguoi-dung/yeu-thich"
-                            label="Danh sách yêu thích"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Danh sách yêu thích
+                                </span>
+                            }
                         />
                         <NavLink
-                            leftSection={<IconDoorExit size="1rem" />}
+                            leftSection={<IconDoorExit size="1.3rem" />}
                             href="/nguoi-dung/thong-tin-tai-khoan"
-                            label="Đăng Xuất"
+                            label={
+                                <span style={{ fontSize: "14.20px" }}>
+                                    Đăng Xuất
+                                </span>
+                            }
                         />
                     </ul>
                 </div>
