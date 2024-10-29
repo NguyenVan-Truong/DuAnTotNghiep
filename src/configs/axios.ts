897513b5abd351bff84f1496e1dbbb30d216c174
NguyenVan-Truong <truongha21062004@gmail.com>
2024-10-29 12:21:52 +0700

    call api products

---
+++ src/configs/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    timeout: 10000,
+});
+console.log(import.meta.env.VITE_BASE_URL);
+export default instance;
+
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
