867e41ddb91f453df5767fd693435f32a7814dc1
NguyenVan-Truong <truongha21062004@gmail.com>
2024-11-04 12:52:11 +0700

    Lien_he

---
+++ src/modals/Information.ts
@@ -0,0 +1,11 @@
+export interface Information {
+    id: number;
+    name: string;
+    phone: string;
+    address: string;
+    map: string;
+    image: string;
+    created_at: string | null;
+    updated_at: string | null;
+    deleted_at: string | null;
+}
