cbd236535146b6481c31d52e544be9a8d1bc2986
luat1410 <luat77555@gmail.com>
2024-11-09 23:59:16 +0700

    update

--- src/pages/Introduce/Introduce.tsx
+++ src/pages/Introduce/Introduce.tsx
@@ -1,26 +1,23 @@
-import axios from "axios";
 import BannerIntroduce from "./Components/Banner/BannerIntroduce";
-import Content from "./Components/Content/Content";
-import ContentPost from "./Components/Content/ContentPost";
-//import Founder from "./Components/Content/Founder";
-import Quality from "./Components/Content/Quality";
 import Values from "./Components/Content/Values";
+import Quality from "./Components/Content/Quality";
 import StoryNew from "./Components/StoryNews/StoryNew";
 import ViewAll from "./Components/ViewAll/ViewAll";
-import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Loader } from "@mantine/core";
 import { AboutPages } from "@/model/AboutPages";
+import instance from "@/configs/axios";
+import Content from "./Components/Content/Content";
+import ContentPost from "./Components/Content/ContentPost";
 
 
 // Hàm gọi API
 const fetchAboutData = async () => {
-  const response = await axios.get('http://127.0.0.1:8000/api/about');
+  const response = await instance.get('/about');
   return response.data;
 };
 
 const Introduce = () => {
-
   // Sử dụng useQuery để gọi API
   const { data, isLoading, error } = useQuery<AboutPages[]>({
     queryKey: ["aboutData"],
@@ -31,28 +28,28 @@ const Introduce = () => {
   if (isLoading) return <Loader />; // Hiển thị loading spinner khi đang tải
   if (error) return <div>Lỗi khi tải dữ liệu giới thiệu</div>; // Hiển thị thông báo lỗi nếu có lỗi
 
-  // Lọc dữ liệu theo id
-  const contentData = data?.find((item) => item.id === 1) || null;
-  const contentPostData = data?.find((item) => item.id === 2) || null;
-  const valuesData = data?.find((item) => item.id === 5) || null;
-  const qualityData = data?.find((item) => item.id === 4) || null;
-
-
   return (
     <>
       {/*Banner*/}
       <BannerIntroduce />
-      {/*Content*/}
-      <Content data={contentData} />
+      {/* Content */}
+      <Content />
       {/*ContentPost*/}
-      <ContentPost data={contentPostData} />
-      {/*<Founder />*/}
-      <Values data={valuesData} />
-      <Quality data={qualityData} />
+      <ContentPost />
+      
+      {data?.map((item, index) => (
+        item.id % 2 === 0 ? (
+          <Values key={index} data={item} />
+        ) : (
+          <Quality key={index} data={item} />
+        )
+      ))}
+
+      {/* Các thành phần khác */}
       <StoryNew />
       <ViewAll />
     </>
-  )
+  );
 }
 
 export default Introduce;
